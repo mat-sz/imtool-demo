af1db9f478fa1585b8a4bf9e3b3284f38375d3b9
Mat Sz <contact@matsz.dev>
2020-03-05 22:23:20 +0100

    added file details

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { ImTool } from 'imtool/lib/ImTool';
+import { fromImage } from 'imtool';
 
 import './App.scss';
 import { SelectFile } from './components/SelectFile';
+import { FileDetails } from './components/FileDetails';
 
 function App() {
     const [ file, setFile ] = useState<File>();
+    const [ tool, setTool ] = useState<ImTool>();
+    const [ inputURL, setInputURL ] = useState<string>();
+    const [ outputURL, setOutputURL ] = useState<string>();
+
+    useEffect(() => {
+        if (file) {
+            fromImage(file).then(
+                (tool) => {
+                    tool.toDataURL().then((url) => setInputURL(url));
+                    setTool(tool);
+                }
+            );
+        }
+    }, [ file ]);
 
     return (
         <div className="App">
-            <h1>imtool demo</h1>
+            <h1>imtool</h1>
             <SelectFile onFile={setFile} />
+            <FileDetails tool={tool} inputURL={inputURL} outputURL={outputURL} />
         </div>
     );
 }
---
+++ src/components/FileDetails.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ImTool } from 'imtool/lib/ImTool';
+
+export interface FileDetailsProps {
+    tool: ImTool | undefined,
+    inputURL: string | undefined,
+    outputURL: string | undefined
+}
+
+export const FileDetails: React.FC<FileDetailsProps> = ({ tool, inputURL, outputURL }) => {
+    if (!tool || !inputURL) return null;
+
+    return (
+        <div className="details">
+            <div>
+                <img src={inputURL} alt="Input" />
+                <div>{ tool.originalWidth }x{ tool.originalHeight }</div>
+            </div>
+            { outputURL ? (
+                <>
+                    <div>
+                        ->
+                    </div>
+                    <div>
+                        <img src={outputURL} alt="Output" />
+                        <div>{ tool.width }x{ tool.height }</div>
+                    </div>
+                </>
+            ) : null}
+        </div>
+    );
+}
